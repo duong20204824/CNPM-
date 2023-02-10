a5b791d7992825719c5153c44bde1a78d026c734
duong20204824 <120170716+duong20204824@users.noreply.github.com>
2023-02-10 23:30:44 +0700

    Initial commit

---
+++ src/main/java/org/idrice24/controllers/AboutUsController.java
@@ -0,0 +1,11 @@
+package org.idrice24.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequestMapping("/")
+public class AboutUsController{
+
+}
---
+++ src/main/java/org/idrice24/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package org.idrice24.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequestMapping("/")
+public class HomeController{
+
+	public HomeController(){
+
+    }
+
+    @GetMapping("home/app")
+    public String homePage(){
+        return "home";
+    }	
+}
